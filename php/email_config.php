2af8e825ee98de597a773ffc930ed310abf71246
Hasan Ahmed <32242140+hasansust@users.noreply.github.com>
2018-08-08 02:26:36 +0600

    Add files via upload

---
+++ php/email_config.php
@@ -0,0 +1,15 @@
+<?php
+	
+	//your email here
+	$to_email = 'hasan.sylh3t@gmail.com';
+	
+	//subject of email you will receive
+	$subject = 'Contact from Website!';	
+	
+	//Error Messages
+	$invalid_name = 'Please enter your name.';
+	$invalid_email = 'Please enter valid e-mail.';
+	$invalid_message = 'Please enter your message.';
+	$sending_error = 'Sorry, we can\'t send your message.';
+
+ ?>
